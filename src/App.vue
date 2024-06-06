a8000435b52f2dbba13d69b39cf5d2040c25b3bb
Kavindu Dilanka <63066789+kavindu10Dilanka@users.noreply.github.com>
2024-06-06 16:31:23 +0530

    last

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <div id="app">
+  
     <DataTable />
-  </div>
+  
 </template>
 
 <script>
